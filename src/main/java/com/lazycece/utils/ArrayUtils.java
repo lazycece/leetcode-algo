98efecf4af65f7586bb68aacf54d952ab77dcc90
lazycece <lazycece@gmail.com>
2022-04-29 00:51:12 +0800

    matrix print

--- src/main/java/com/lazycece/utils/ArrayUtils.java
+++ src/main/java/com/lazycece/utils/ArrayUtils.java
@@ -28,4 +28,13 @@ public class ArrayUtils {
         }
         System.out.println();
     }
+
+    public static void print(int[][] m) {
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                System.out.print(m[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
 }
