611e2a009e73fb526b72432a4dfbd1b4a22df704
lazycece <lazycece@gmail.com>
2022-05-04 22:05:19 +0800

    Lc142LinkedListCycleii_t

---
+++ src/main/java/com/lazycece/algo/lc_101_150/lc142_LinkedListCycleii/Lc142LinkedListCycleii.java
@@ -0,0 +1,50 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_101_150.lc142_LinkedListCycleii;
+
+import com.lazycece.model.ListNode;
+
+/**
+ * @author lazycece
+ * @date 2022/5/4
+ */
+public class Lc142LinkedListCycleii {
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                ListNode p1 = fast;
+                ListNode p2 = head;
+                // 两个指针，从头结点和相遇结点，各走一步，直到相遇，相遇点即为环入口
+                while (p1 != p2) {
+                    p1 = p1.next;
+                    p2 = p2.next;
+                }
+                return p1;
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
