4a3e8763cc5f71b40745c4fe4ec6746f003a4c90
lazycece <lazycece@gmail.com>
2022-05-04 21:03:52 +0800

    Lc237DeleteNodeInALinkedList2

---
+++ src/main/java/com/lazycece/algo/lc_201_250/lc237_DeleteNodeInALinkedList/Lc237DeleteNodeInALinkedList2.java
@@ -0,0 +1,32 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_201_250.lc237_DeleteNodeInALinkedList;
+
+import com.lazycece.model.ListNode;
+
+/**
+ * @author lazycece
+ * @date 2022/5/4
+ */
+public class Lc237DeleteNodeInALinkedList2 {
+
+    public void deleteNode(ListNode node) {
+        // 赋值转换，删除当前节点的下一个节点
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
