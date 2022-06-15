ec67a6e28d5b0092316a94da0714586880ca87e9
lazycece <lazycece@gmail.com>
2022-06-15 21:46:46 +0800

    Lc18FourSum

--- src/main/java/com/lazycece/algo/lc_1_50/lc18_4sum/Lc18FourSum.java
+++ src/main/java/com/lazycece/algo/lc_1_50/lc18_4sum/Lc18FourSum.java
@@ -16,6 +16,10 @@
 
 package com.lazycece.algo.lc_1_50.lc18_4sum;
 
+import com.lazycece.utils.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -25,6 +29,45 @@ import java.util.List;
 public class Lc18FourSum {
 
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        if (nums.length < 4) {
+            return list;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                int l = j + 1;
+                int r = nums.length - 1;
+                while (l < r) {
+                    int temp = nums[i] + nums[j] + nums[l] + nums[r];
+                    if (temp > target) {
+                        r--;
+                    } else if (temp < target) {
+                        l++;
+                    } else {
+                        list.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        while (l < r && nums[l] == nums[l + 1]) l++;
+                        while (l < r && nums[r] == nums[r - 1]) r--;
+                        l++;
+                        r--;
+                    }
+                }
+            }
+        }
+        return list;
+    }
 
+    public static void main(String[] args) {
+        Lc18FourSum lc = new Lc18FourSum();
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        ArrayUtils.print(lc.fourSum(nums, 0));
+        int[] nums1 = {2, 2, 2, 2, 2};
+        ArrayUtils.print(lc.fourSum(nums1, 8));
     }
 }
