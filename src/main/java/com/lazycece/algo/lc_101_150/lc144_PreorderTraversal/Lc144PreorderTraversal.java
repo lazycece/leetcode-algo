185bd2c6aaa374dc247fe2d27ca5b93cca7a3be6
lazycece <lazycece@gmail.com>
2022-06-29 22:02:54 +0800

    Lc144PreorderTraversal

---
+++ src/main/java/com/lazycece/algo/lc_101_150/lc144_PreorderTraversal/Lc144PreorderTraversal.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_101_150.lc144_PreorderTraversal;
+
+import com.lazycece.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 递归思路
+ *
+ * @author lazycece
+ * @date 2022/6/26
+ */
+public class Lc144PreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.add(root.val);
+        list.addAll(preorderTraversal(root.left));
+        list.addAll(preorderTraversal(root.right));
+        return list;
+    }
+
+}
