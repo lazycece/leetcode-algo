fd95a89c2dad97b21913fef4b4d0a296205c4163
lazycece <lazycece@gmail.com>
2022-06-30 01:01:40 +0800

    Lc145PostorderTraversal

---
+++ src/main/java/com/lazycece/algo/lc_101_150/lc145_PostorderTraversal/Lc145PostorderTraversal.java
@@ -0,0 +1,40 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_101_150.lc145_PostorderTraversal;
+
+import com.lazycece.model.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author lazycece
+ * @date 2022/6/29
+ */
+public class Lc145PostorderTraversal {
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        list.addAll(postorderTraversal(root.left));
+        list.addAll(postorderTraversal(root.right));
+        list.add(root.val);
+        return list;
+    }
+}
