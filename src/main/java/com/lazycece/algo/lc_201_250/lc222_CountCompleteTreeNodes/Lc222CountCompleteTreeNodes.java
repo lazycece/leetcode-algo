26cdc803e7b8e486472b627b60ff7a18c75fde6f
lazycece <lazycece@gmail.com>
2022-07-02 17:50:04 +0800

    Lc222CountCompleteTreeNodes

---
+++ src/main/java/com/lazycece/algo/lc_201_250/lc222_CountCompleteTreeNodes/Lc222CountCompleteTreeNodes.java
@@ -0,0 +1,33 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_201_250.lc222_CountCompleteTreeNodes;
+
+import com.lazycece.model.TreeNode;
+
+/**
+ * 递归
+ *
+ * @author lazycece
+ * @date 2022/7/2
+ */
+public class Lc222CountCompleteTreeNodes {
+
+    public int countNodes(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
