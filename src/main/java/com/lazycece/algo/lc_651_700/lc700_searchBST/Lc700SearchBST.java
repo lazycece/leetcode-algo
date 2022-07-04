79e67fea6a302802a0ccfe1d7a27a54e8bf98d03
lazycece <lazycece@gmail.com>
2022-07-04 15:55:46 +0800

    Lc700SearchBST

---
+++ src/main/java/com/lazycece/algo/lc_651_700/lc700_searchBST/Lc700SearchBST.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_651_700.lc700_searchBST;
+
+import com.lazycece.model.TreeNode;
+
+/**
+ * @author lazycece
+ * @date 2022/7/4
+ */
+public class Lc700SearchBST {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        } else if (root.val == val) {
+            return root;
+        } else if (root.val < val) {
+            return searchBST(root.right, val);
+        } else {
+            return searchBST(root.left, val);
+        }
+    }
+}
