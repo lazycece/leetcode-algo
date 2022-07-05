82c5cb66875d10b08474767441a1f326b97a6e78
lazycece <lazycece@gmail.com>
2022-07-05 18:09:50 +0800

    Lc347TopKFrequent2

---
+++ src/main/java/com/lazycece/algo/lc_301_350/lc347_TopKFrequent/Lc347TopKFrequent2.java
@@ -0,0 +1,57 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_301_350.lc347_TopKFrequent;
+
+import com.lazycece.utils.ArrayUtils;
+
+import java.util.*;
+
+/**
+ * @author lazycece
+ * @date 2022/6/25
+ */
+public class Lc347TopKFrequent2 {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] result = new int[k];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        Set<Map.Entry<Integer, Integer>> entries = map.entrySet();
+        // 根据map的value值正序排，相当于一个小顶堆
+        PriorityQueue<Map.Entry<Integer, Integer>> queue = new PriorityQueue<>((o1, o2) -> o1.getValue() - o2.getValue());
+        for (Map.Entry<Integer, Integer> entry : entries) {
+            queue.offer(entry);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+        }
+        for (int i = k - 1; i >= 0; i--) {
+            result[i] = queue.poll().getKey();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Lc347TopKFrequent2 lc = new Lc347TopKFrequent2();
+        int[] nums = {1, 1, 1, 2, 2, 2, 3};
+        int k = 2;
+        ArrayUtils.print(lc.topKFrequent(nums, k));
+    }
+}
