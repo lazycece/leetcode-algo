3a3c1b78fc2969a2c9d61264a1662e1f31aac67a
lazycece <lazycece@gmail.com>
2022-07-06 18:38:09 +0800

    Lc96NumTrees

---
+++ src/main/java/com/lazycece/algo/lc_51_100/lc96_numTrees/Lc96NumTrees.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_51_100.lc96_numTrees;
+
+/**
+ * @author lazycece
+ * @date 2022/7/6
+ */
+public class Lc96NumTrees {
+
+    public int numTrees(int n) {
+
+        // f(3) = f(0)*f(2) + f(1)*f(1) +  f(2)*f(0)
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+}
