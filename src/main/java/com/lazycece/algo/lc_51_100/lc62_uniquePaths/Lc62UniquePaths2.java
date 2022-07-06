519776588328a4695998f1bca0065af165b8deb4
lazycece <lazycece@gmail.com>
2022-07-06 16:56:30 +0800

    Lc62UniquePaths

---
+++ src/main/java/com/lazycece/algo/lc_51_100/lc62_uniquePaths/Lc62UniquePaths2.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_51_100.lc62_uniquePaths;
+
+import java.util.Arrays;
+
+/**
+ * @author lazycece
+ * @date 2022/7/6
+ */
+public class Lc62UniquePaths2 {
+
+    //按行计算
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[m];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+}
