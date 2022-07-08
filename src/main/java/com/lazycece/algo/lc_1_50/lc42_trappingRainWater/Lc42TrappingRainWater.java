d3fea9185a3f3e6862cbafeffad6a36b04ea016c
lazycece <lazycece@gmail.com>
2022-07-09 00:40:53 +0800

    Lc40CombinationSum2

---
+++ src/main/java/com/lazycece/algo/lc_1_50/lc42_trappingRainWater/Lc42TrappingRainWater.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_1_50.lc42_trappingRainWater;
+
+/**
+ * @author lazycece
+ * @date 2022/7/7
+ */
+public class Lc42TrappingRainWater {
+
+    public int trap(int[] height) {
+        // TODO: 2022/7/8
+       return 0;
+    }
+
+    public static void main(String[] args) {
+        Lc42TrappingRainWater lc = new Lc42TrappingRainWater();
+//        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int[] height = {4,2,0,3,2,5};
+        System.out.println(lc.trap(height));
+
+    }
+}
