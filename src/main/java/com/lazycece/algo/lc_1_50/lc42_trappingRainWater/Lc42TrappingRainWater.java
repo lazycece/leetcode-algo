b513a38fefbed5e50da8c9c8d3eb3ff4c7681bb0
lazycece <lazycece@gmail.com>
2022-07-15 11:48:12 +0800

    Lc42TrappingRainWater dp

--- src/main/java/com/lazycece/algo/lc_1_50/lc42_trappingRainWater/Lc42TrappingRainWater.java
+++ src/main/java/com/lazycece/algo/lc_1_50/lc42_trappingRainWater/Lc42TrappingRainWater.java
@@ -17,20 +17,37 @@
 package com.lazycece.algo.lc_1_50.lc42_trappingRainWater;
 
 /**
+ * 动态规划
+ * f(i) = min(i_left_high,i_right_high)-hight[i]
+ *
  * @author lazycece
  * @date 2022/7/7
  */
 public class Lc42TrappingRainWater {
 
     public int trap(int[] height) {
-        // TODO: 2022/7/8
-       return 0;
+        int[] lMaxH = new int[height.length];
+        lMaxH[0] = 0;
+        int[] rMaxH = new int[height.length];
+        rMaxH[height.length - 1] = 0;
+        for (int i = height.length - 2; i >= 0; i--) {
+            rMaxH[i] = Math.max(rMaxH[i + 1], height[i + 1]);
+        }
+        int sum = 0;
+        for (int i = 1; i < height.length; i++) {
+            lMaxH[i] = Math.max(height[i - 1], lMaxH[i - 1]);
+            int tmp = Math.min(lMaxH[i], rMaxH[i]) - height[i];
+            if (tmp > 0) {
+                sum += tmp;
+            }
+        }
+        return sum;
     }
 
     public static void main(String[] args) {
         Lc42TrappingRainWater lc = new Lc42TrappingRainWater();
 //        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int[] height = {4,2,0,3,2,5};
+        int[] height = {4, 2, 0, 3, 2, 5};
         System.out.println(lc.trap(height));
 
     }
