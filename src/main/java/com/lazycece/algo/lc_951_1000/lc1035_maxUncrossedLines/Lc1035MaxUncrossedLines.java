b8cebcdf86d3be0051c5993d972bfe5c4ce1f777
lazycece <lazycece@gmail.com>
2022-07-16 18:05:56 +0800

    Lc1035MaxUncrossedLines

---
+++ src/main/java/com/lazycece/algo/lc_951_1000/lc1035_maxUncrossedLines/Lc1035MaxUncrossedLines.java
@@ -0,0 +1,44 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_951_1000.lc1035_maxUncrossedLines;
+
+/**
+ * 同1143：最长公共子序列
+ *
+ * @author lazycece
+ * @date 2022/7/16
+ */
+public class Lc1035MaxUncrossedLines {
+
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int[][] dp = new int[n1 + 1][n2 + 1];
+        int res = 0;
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    res = Math.max(res, dp[i][j]);
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return res;
+    }
+}
