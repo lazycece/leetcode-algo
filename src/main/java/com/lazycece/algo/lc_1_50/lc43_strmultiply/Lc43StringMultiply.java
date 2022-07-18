53f282db752a352407665eff38c4d18840138d2b
lazycece <lazycece@gmail.com>
2022-07-19 00:33:25 +0800

    Lc43StringMultiply

--- src/main/java/com/lazycece/algo/lc_1_50/lc43_strmultiply/Lc43StringMultiply.java
+++ src/main/java/com/lazycece/algo/lc_1_50/lc43_strmultiply/Lc43StringMultiply.java
@@ -21,10 +21,75 @@ package com.lazycece.algo.lc_1_50.lc43_strmultiply;
  * @date 2022/2/17
  */
 public class Lc43StringMultiply {
-
     public String multiply(String num1, String num2) {
-        // TODO: 2022/4/26  
-        return num1;
+        if ("0".equals(num1) || "0".equals(num2)) return "0";
+        int offset = 0;
+        String res = "";
+        for (int i = num1.length() - 1; i >= 0; i--) {
+            int ofs = 0;
+            String forAdd = "";
+            for (int j = num2.length() - 1; j >= 0; j--) {
+                String tmp = multiply(num1.charAt(i), num2.charAt(j));
+                forAdd = add(tmp, forAdd, ofs);
+                ofs++;
+            }
+            res = add(forAdd, res, offset);
+            offset++;
+        }
+        return res;
+    }
+
+    private String multiply(char a, char b) {
+        int intA = Integer.parseInt(String.valueOf(a));
+        int intB = Integer.parseInt(String.valueOf(b));
+        return String.valueOf(intA * intB);
+    }
+
+    private String add(String ofsNum, String num, int ofs) {
+        for (int i = 1; i <= ofs; i++) {
+            ofsNum += "0";
+        }
+        int up = 0;
+        String res = "";
+        int ofsNumOfs = ofsNum.length() - num.length();
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int intA = Integer.parseInt(String.valueOf(num.charAt(i)));
+            int intB = Integer.parseInt(String.valueOf(ofsNum.charAt(i + ofsNumOfs)));
+            int sum = intA + intB + up;
+            if (sum >= 10) {
+                up = 1;
+                res = String.valueOf(sum % 10) + res;
+            } else {
+                up = 0;
+                res = String.valueOf(sum) + res;
+            }
+        }
+        if (ofsNumOfs > 0) {
+            for (int i = ofsNumOfs - 1; i >= 0; i--) {
+                int tmp = Integer.parseInt(String.valueOf(ofsNum.charAt(i)));
+                int sum = tmp + up;
+                if (sum >= 10) {
+                    up = 1;
+                    res = String.valueOf(sum % 10) + res;
+                } else {
+                    up = 0;
+                    res = String.valueOf(sum) + res;
+                }
+            }
+        }
+        if (up == 1) {
+            // 注意最后的进位
+            res = "1" + res;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Lc43StringMultiply lc = new Lc43StringMultiply();
+//        System.out.println(lc.multiply("2", "3"));
+//        System.out.println(lc.multiply("123", "456"));
+        System.out.println(lc.multiply("0", "9"));
+        System.out.println(lc.multiply("9133", "0"));
     }
 
 }
