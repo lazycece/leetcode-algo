b72b7b9b4fe13b6a356ff967401fc40d9d5bab99
lazycece <lazycece@gmail.com>
2022-08-03 20:09:21 +0800

    Lc283MoveZeroes2

---
+++ src/main/java/com/lazycece/algo/lc_251_300/lc283_moveZeroes/Lc283MoveZeroes2.java
@@ -0,0 +1,39 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_251_300.lc283_moveZeroes;
+
+/**
+ * @author lazycece
+ * @date 2022/8/3
+ */
+public class Lc283MoveZeroes2 {
+
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int l = 0, r = 0;
+        while (r < n) {
+            if (nums[r] != 0) {
+                int t = nums[l];
+                nums[l] = nums[r];
+                nums[r] = t;
+                l++;
+            }
+            r++;
+        }
+
+    }
+}
