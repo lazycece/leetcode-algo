04d1d681276ebba6b8c53691f7e09f0bd32db94c
lazycece <lazycece@gmail.com>
2022-08-09 15:27:30 +0800

    Lc198Rob2

---
+++ src/main/java/com/lazycece/algo/lc_151_200/lc198_rob/Lc198Rob2.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_151_200.lc198_rob;
+
+/**
+ * dp
+ * f[i] = max(f[i-1],f[i-2]+a[i]);
+ *
+ * @author lazycece
+ * @date 2022/8/9
+ */
+public class Lc198Rob2 {
+
+    public int rob(int[] nums) {
+        int len = nums.length;
+        int[] dp = new int[len + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 2; i <= len; i++) {
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+        }
+        return dp[len];
+    }
+}
