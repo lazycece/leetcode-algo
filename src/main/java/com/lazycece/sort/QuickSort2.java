20a6e5cb326419a0ac3c3f1d82097f31064ca57e
lazycece <lazycece@gmail.com>
2022-08-09 12:25:29 +0800

    quicksort

---
+++ src/main/java/com/lazycece/sort/QuickSort2.java
@@ -0,0 +1,51 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.sort;
+
+import com.lazycece.utils.ArrayUtils;
+
+/**
+ * @author lazycece
+ * @date 2022/7/5
+ */
+public class QuickSort2 {
+
+    public void quickSort(int[] nums, int low, int high) {
+        int temp = nums[low];
+        int l = low, r = high;
+        while (l < r) {
+            while (l < r && nums[r] >= temp) r--;
+            nums[l] = nums[r];
+            while (l < r && nums[l] <= temp) l++;
+            nums[r] = nums[l];
+        }
+        nums[l] = temp;
+        if (l > low) {
+            quickSort(nums, low, l - 1);
+        }
+        if (r < high) {
+            quickSort(nums, r + 1, high);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {12, 20, 5, 16, 15, 1, 30, 45};
+        QuickSort2 sort = new QuickSort2();
+        sort.quickSort(nums, 0, nums.length - 1);
+        ArrayUtils.print(nums);
+    }
+}
