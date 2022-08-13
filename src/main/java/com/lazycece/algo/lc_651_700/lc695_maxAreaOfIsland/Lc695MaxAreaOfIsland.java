79819b3e0757e21452a6cebfab247cd6bec39671
lazycece <lazycece@gmail.com>
2022-08-13 20:48:18 +0800

    Lc695MaxAreaOfIsland

---
+++ src/main/java/com/lazycece/algo/lc_651_700/lc695_maxAreaOfIsland/Lc695MaxAreaOfIsland.java
@@ -0,0 +1,52 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_651_700.lc695_maxAreaOfIsland;
+
+/**
+ * @author lazycece
+ * @date 2022/8/13
+ */
+public class Lc695MaxAreaOfIsland {
+    private int m;
+    private int n;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        m = grid.length;
+        n = grid[0].length;
+        int maxArea = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) return 0;
+        if (grid[i][j] == 0) return 0;
+        grid[i][j] = 0;
+        int area = 1;
+        area += dfs(grid, i - 1, j);
+        area += dfs(grid, i + 1, j);
+        area += dfs(grid, i, j - 1);
+        area += dfs(grid, i, j + 1);
+        return area;
+    }
+}
