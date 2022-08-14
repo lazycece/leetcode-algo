355f2d6964b55c542d1a6533d7225e67047ff891
lazycece <lazycece@gmail.com>
2022-08-14 23:32:11 +0800

    lc196 sql

---
+++ src/main/java/com/lazycece/algo/lc_151_200/lc197_sql/lc197.sql
@@ -0,0 +1,22 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+select w2.id as Id
+from Weather w1
+         inner join Weather w2
+                    on datediff(w2.recordDate, w1.recordDate) = 1
+                        and w2.temperature > w1.temperature
+;
