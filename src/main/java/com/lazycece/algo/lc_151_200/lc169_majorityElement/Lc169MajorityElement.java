db4d9f9caa149fd36af1bb8623b18b0c3863a262
lazycece <lazycece@gmail.com>
2022-08-17 21:34:39 +0800

    Lc169MajorityElement

---
+++ src/main/java/com/lazycece/algo/lc_151_200/lc169_majorityElement/Lc169MajorityElement.java
@@ -0,0 +1,39 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_151_200.lc169_majorityElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lazycece
+ * @date 2022/8/17
+ */
+public class Lc169MajorityElement {
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> et : map.entrySet()) {
+            if (nums.length / 2 < et.getValue()) return et.getKey();
+        }
+        return 0;
+    }
+}
