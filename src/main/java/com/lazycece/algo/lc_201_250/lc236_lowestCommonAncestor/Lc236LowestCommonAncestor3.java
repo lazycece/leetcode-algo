a2039ba964dc0d855ff65929659a65f05097be60
lazycece <lazycece@gmail.com>
2022-08-18 19:53:38 +0800

    Lc236LowestCommonAncestor3

---
+++ src/main/java/com/lazycece/algo/lc_201_250/lc236_lowestCommonAncestor/Lc236LowestCommonAncestor3.java
@@ -0,0 +1,35 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_201_250.lc236_lowestCommonAncestor;
+
+import com.lazycece.model.TreeNode;
+
+/**
+ * @author lazycece
+ * @date 2022/7/4
+ */
+public class Lc236LowestCommonAncestor3 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p, q);
+        TreeNode r = lowestCommonAncestor(root.right, p, q);
+        if (l != null && r != null) return root;
+        if (l == null) return r;
+        return l;
+    }
+}
