ec8f79cb0c55b91fd7642085df2a1814a24cf7c1
lazycece <lazycece@gmail.com>
2022-08-21 17:19:19 +0800

    Lc560SubarraySum2

---
+++ src/main/java/com/lazycece/algo/lc_551_600/lc560_subarraySum/Lc560SubarraySum2.java
@@ -0,0 +1,43 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_551_600.lc560_subarraySum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lazycece
+ * @date 2022/8/21
+ */
+public class Lc560SubarraySum2 {
+
+    public int subarraySum(int[] nums, int k) {
+        int res = 0;
+        // 以i为结尾的数组有满足要求子数组的个数
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0;
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
