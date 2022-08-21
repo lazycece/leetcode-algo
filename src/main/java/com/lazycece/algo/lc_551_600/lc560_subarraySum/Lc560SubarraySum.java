a03c72baa936d3b22e725420de240c0a8c82bbad
lazycece <lazycece@gmail.com>
2022-08-21 16:23:03 +0800

    Lc560SubarraySum

---
+++ src/main/java/com/lazycece/algo/lc_551_600/lc560_subarraySum/Lc560SubarraySum.java
@@ -0,0 +1,36 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_551_600.lc560_subarraySum;
+
+/**
+ * @author lazycece
+ * @date 2022/8/21
+ */
+public class Lc560SubarraySum {
+
+    public int subarraySum(int[] nums, int k) {
+        int res = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) res++;
+            }
+        }
+        return res;
+    }
+}
