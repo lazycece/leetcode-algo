3f8d1dfc1c5d20de350734de14515988f58e2af3
lazycece <lazycece@gmail.com>
2022-08-21 15:00:25 +0800

    Lc543DiameterOfBinaryTree

---
+++ src/main/java/com/lazycece/algo/lc_501_550/lc543_diameterOfBinaryTree/Lc543DiameterOfBinaryTree.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_501_550.lc543_diameterOfBinaryTree;
+
+import com.lazycece.model.TreeNode;
+
+/**
+ * @author lazycece
+ * @date 2022/8/21
+ */
+public class Lc543DiameterOfBinaryTree {
+
+    private int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        helper(root);
+        return max;
+    }
+
+    private int helper(TreeNode root) {
+        if (root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        max = Math.max(max, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
