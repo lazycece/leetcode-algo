441447b14a378ae45c342c77818eae1037262b3b
lazycece <lazycece@gmail.com>
2022-08-22 12:28:15 +0800

    Lc287FindDuplicate

---
+++ src/main/java/com/lazycece/algo/lc_251_300/lc287_findDuplicate/Lc287FindDuplicate3.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_251_300.lc287_findDuplicate;
+
+/**
+ * @author lazycece
+ * @date 2022/8/22
+ */
+public class Lc287FindDuplicate3 {
+
+    public int findDuplicate(int[] nums) {
+        //1,3,4,2,2
+        //0,1,2,3,4
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
