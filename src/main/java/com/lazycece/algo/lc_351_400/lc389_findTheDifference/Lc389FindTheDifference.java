e5bf3a19f6f6f825c28794f496b28b840adb45fa
lazycece <lazycece@gmail.com>
2022-08-26 20:16:37 +0800

    Lc389FindTheDifference

---
+++ src/main/java/com/lazycece/algo/lc_351_400/lc389_findTheDifference/Lc389FindTheDifference.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_351_400.lc389_findTheDifference;
+
+/**
+ * 位运算
+ *
+ * @author lazycece
+ * @date 2022/8/26
+ */
+public class Lc389FindTheDifference {
+
+    public char findTheDifference(String s, String t) {
+        int res = 0;
+        for (int i = 0; i < s.length(); i++) {
+            res ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); i++) {
+            res ^= t.charAt(i);
+        }
+        return (char) res;
+    }
+}
