baa6feba450e05734c05307c687142cb3c305b76
lazycece <lazycece@gmail.com>
2022-08-27 20:16:04 +0800

    Lc137SingleNumberII

---
+++ src/main/java/com/lazycece/algo/lc_101_150/lc137_singleNumberII/Lc137SingleNumberII.java
@@ -0,0 +1,37 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_101_150.lc137_singleNumberII;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author lazycece
+ * @date 2022/8/26
+ */
+public class Lc137SingleNumberII {
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (Integer i : nums)
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        for (Map.Entry<Integer, Integer> en : map.entrySet()) {
+            if (en.getValue() == 1) return en.getKey();
+        }
+        throw new IllegalArgumentException("param error");
+    }
+}
