adcb5335baa65635d31e5b518a7e406a2bc96765
lazycece <lazycece@gmail.com>
2022-08-30 22:28:16 +0800

    Lc448FindDisappearedNumbers

---
+++ src/main/java/com/lazycece/algo/lc_401_450/lc448_findDisappearedNumbers/Lc448FindDisappearedNumbers2.java
@@ -0,0 +1,41 @@
+/*
+ *    Copyright 2022 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_401_450.lc448_findDisappearedNumbers;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @author lazycece
+ * @date 2022/8/30
+ */
+public class Lc448FindDisappearedNumbers2 {
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            if (!set.contains(i)) res.add(i);
+        }
+        return res;
+    }
+}
