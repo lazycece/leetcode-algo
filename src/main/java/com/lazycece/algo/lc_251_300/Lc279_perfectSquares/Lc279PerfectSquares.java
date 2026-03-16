96961bb3fe9dae3671b6005c19c025d3618e7395
lazycece <lazycece@gmail.com>
2026-03-16 16:03:40 +0800

    Lc279PerfectSquares

---
+++ src/main/java/com/lazycece/algo/lc_251_300/Lc279_perfectSquares/Lc279PerfectSquares.java
@@ -0,0 +1,39 @@
+/*
+ *    Copyright 2026 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_251_300.Lc279_perfectSquares;
+
+/**
+ * @author lazycece
+ * @date 2026/3/16
+ */
+public class Lc279PerfectSquares {
+
+    public int numSquares(int n) {
+        // 公式: f[n] = f[n-j*j] + 1;
+        //  1<= j <= 根号n  => j*j>=1 && j*j<=n
+        int[] f = new int[n + 1];
+        f[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, f[i - j * j]);
+            }
+            f[i] = min + 1;
+        }
+        return f[n];
+    }
+}
