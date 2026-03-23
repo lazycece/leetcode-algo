ba6d6bd951cbd42baf52541665b7103b3207e933
lazycece <lazycece@gmail.com>
2026-03-23 18:35:19 +0800

    Lc64MinimumPathSum

---
+++ src/main/java/com/lazycece/algo/lc_51_100/lc64_minimumPathSum/Lc64MinimumPathSum.java
@@ -0,0 +1,45 @@
+/*
+ *    Copyright 2026 lazycece<lazycece@gmail.com>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.lazycece.algo.lc_51_100.lc64_minimumPathSum;
+
+/**
+ * @author lazycece
+ * @date 2026/3/23
+ */
+public class Lc64MinimumPathSum {
+    
+    public int minPathSum(int[][] grid) {
+        // 动态规划，公式: dp[i][j] = Math.min(dp[i-1][j],dp[i][j-1])+grid[i][j];
+        int m = grid.length, n = grid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = grid[0][0];
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
