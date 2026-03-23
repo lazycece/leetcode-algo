ccdb977d11499849d328574937e9222ac1f871a3
lazycece <lazycece@gmail.com>
2026-03-23 21:52:38 +0800

    mdf

--- src/main/java/com/lazycece/algo/lc_51_100/lc72_minDistance/Lc72MinDistance.java
+++ src/main/java/com/lazycece/algo/lc_51_100/lc72_minDistance/Lc72MinDistance.java
@@ -28,14 +28,19 @@ public class Lc72MinDistance {
         int[][] dp = new int[n1 + 1][n2 + 1];
         for (int i = 0; i <= n1; i++) dp[i][0] = i;
         for (int i = 0; i <= n2; i++) dp[0][i] = i;
+
         for (int i = 1; i <= n1; i++) {
             for (int j = 1; j <= n2; j++) {
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    // 如果字符相等，则当前操作等于上一个最小操作数
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
+                    // "i-1字符串插入字符操作" 与"i字符串的替换操作" 与" i字符串的删除操作" 的最小操作数
                     dp[i][j] = Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1])) + 1;
                 }
+                // System.out.print(dp[i][j]+",");
             }
+            // System.out.println();
         }
         return dp[n1][n2];
     }
